c520d63731243b1740f4ee6cbe6c9cc5f4823be3
Raven <craven@crowz.kr>
2016-03-11 14:37:54 +0900

    pthread Test/// win 10

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 /*
  * main.c
  *
- *  Created on: Mar 9, 2016
- *      Author: araven
+ *  Created on: 2016. 3. 11.
+ *      Author: crave
  */
 
 
 #include <stdio.h>
+#include <pthread.h>
+
+static void threadTest(void)
+{
+	printf("Thread!!\n");
+}
 
 int main(void)
 {
+	pthread_t thread;
 	printf("hello Word\n");
+
+	pthread_create(&thread,NULL,threadTest,NULL);
+	pthread_join(thread,NULL);
+	printf("threadTestDone\n");
+	return 0;
 }
