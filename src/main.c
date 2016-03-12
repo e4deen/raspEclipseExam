1136398514d00abecf58b5d9da0fbc6ebfb343b0
Raven <craven@crowz.kr>
2016-03-12 10:12:48 +0900

    Rpi 2 BCM 0 pin togle Test

--- src/main.c
+++ src/main.c
@@ -10,6 +10,9 @@
 #include <pthread.h>
 #include "wiringPi/wiringPi.h"
 
+
+#define LED 0	///BCM 0
+
 static void *threadTest(void *argument)
 {
 	printf("Thread!!\n");
@@ -22,10 +25,18 @@ int main(void)
 {
 	pthread_t thread;
     wiringPiSetupGpio () ;
+    pinMode (LED, OUTPUT) ;
 	printf("hello Word\n");
 
 	pthread_create(&thread,NULL,threadTest,NULL);
 	pthread_join(thread,NULL);
 	printf("threadTestDone\n");
+	for(;;)
+	{
+	    digitalWrite (LED, HIGH) ;	// On
+	    delay (1) ;		// mS
+	    digitalWrite (LED, LOW) ;	// Off
+	    delay (1) ;
+	}
 	return 0;
 }
