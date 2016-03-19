605cec9dea8fcad086bf64d49515de67aefc361c
Raven <craven@crowz.kr>
2016-03-19 15:51:48 +0900

    i2c module test

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "wiringPi/wiringPi.h"
+#include "wiringPi/wiringPiI2C.h"
 
 
 #define LED 0	///BCM 0
@@ -24,13 +25,49 @@ static void *threadTest(void *argument)
 int main(void)
 {
 	pthread_t thread;
-    wiringPiSetupGpio () ;
+	wiringPiSetupGpio () ;
     pinMode (LED, OUTPUT) ;
 	printf("hello Word\n");
 
 	pthread_create(&thread,NULL,threadTest,NULL);
 	pthread_join(thread,NULL);
 	printf("threadTestDone\n");
+/*
+
+	int i = 0;
+	for(i=0; i<0x77;i++)
+	{
+		int fd = wiringPiI2CSetup(i);
+		printf("fd [%x] index[%x]\n",fd, i);
+	}
+
+*/
+	int fd = wiringPiI2CSetup(0x40);
+	printf("fd [%x]\n",fd);
+
+
+	wiringPiI2CWriteReg8(fd,0xfa,0);
+	wiringPiI2CWriteReg8(fd,0xfb,0);
+	wiringPiI2CWriteReg8(fd,0xfc,0);
+	wiringPiI2CWriteReg8(fd,0xfd,0);
+
+	wiringPiI2CWriteReg8(fd,0x01,0x04);
+	wiringPiI2CWriteReg8(fd,0x00,0x01);
+
+	int reg = wiringPiI2CReadReg8(fd,0x00);
+	printf("reg 0  [%x]\n",reg);
+	reg = wiringPiI2CReadReg8(fd,0x01);
+	printf("reg 1 [%x]\n",reg);
+	reg = wiringPiI2CReadReg8(fd,0x02);
+	printf("reg 2 [%x]\n",reg);
+
+/*	int i = 0;
+	for(i=0; i<20;i++)
+	{
+		reg = wiringPiI2CReadReg8(fd,i);
+		printf("reg %d [%x]\n",i,reg);
+	}*/
+
 	for(;;)
 	{
 	    digitalWrite (LED, HIGH) ;	// On
